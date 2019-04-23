377e762c05bbf9c6024d284fd86cfe6f194ff5fc
myriamDominguez <myriamlarson@hotmail.com>
2019-04-23 18:15:19 +0200

    Ejercicios de alertas con script interno y externo

---
+++ alert.js
@@ -0,0 +1,9 @@
+var hola = 'variable 1'
+var suma = ' +'
+var adios = 'variable 2'
+var resultado = '= Hola y adiós'
+var total = hola + suma + adios + resultado
+
+alert( 'Hola, mundo! Im JavaScript' );
+alert( 'Esto es otra alerta mediante script externo' );
+alert(total)
