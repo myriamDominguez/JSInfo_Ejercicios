d816f1232255f14c81199cc5dfdcfc99f4c434be
myriamDominguez <myriamlarson@hotmail.com>
2019-04-24 19:15:25 +0200

    Ejercicios Code structure. Semicolon y comments.

--- alert.js
+++ alert.js
@@ -1,3 +1,5 @@
+// FUNDAMENTALS SCRIPT BUILDING - HELLO, WORLD
+
 var hola = 'variable 1'
 var suma = ' +'
 var adios = 'variable 2'
@@ -6,4 +8,12 @@ var total = hola + suma + adios + resultado
 
 alert( 'Hola, mundo! Im JavaScript' );
 alert( 'Esto es otra alerta mediante script externo' );
-alert(total)
+alert(total);
+
+
+// FUNDAMENTALS SCRIPT BUILDING - CODE STRUCTURE
+
+// alert("There will be an error")
+// alert("There will be an error")[1, 2].forEach(alert)
+alert("All fine now");
+[1, 2, 3].forEach(alert)
