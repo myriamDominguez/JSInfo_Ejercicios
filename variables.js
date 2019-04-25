1555067a69182ca041b8938e9fae2c7a472831eb
myriamDominguez <myriamlarson@hotmail.com>
2019-04-25 18:20:24 +0200

    Ejercicio Variables y Constants.

---
+++ variables.js
@@ -0,0 +1,22 @@
+// I'll use the Modern Mode
+
+'use strict'
+
+
+// Declarar dos variables
+
+let user = 'Myriam';
+let admin = user;
+alert(admin);
+
+
+// Giving the right name
+
+let ourPlanetName = 'Earth';
+let currentUserName = 'Myriam';
+
+
+// Uppercase const
+const BIRTHDAY = '09.06.1989';
+const age = 'calculated';
+alert(BIRTHDAY);
